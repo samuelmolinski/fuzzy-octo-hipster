207d366d630d509db52791141e3976f5b4e048a3
Samuel <sjmolinski@gmail.com>
2012-10-24 08:34:05 -0200

    -added Yii framework

---
+++ New folder/Class_Number.php
@@ -0,0 +1,33 @@
+<?php
+	class Number {
+		public $n; // the numbers value (usually from 1-60)
+		public $D; // which tens group it belongs. values 0-5
+		public $DF; // final digit
+
+		public function Number($v) {
+			$this->$n = $v;
+			$this->set_D();
+		}
+		public function set_D(){
+			switch($this->$n) {
+				case ((1 <= $v)&&($v <=10)):
+					$this->D = 0;
+					break;
+				case ((11 <= $v)&&($v <=20)):
+					$this->D = 1;
+					break;
+				case ((21 <= $v)&&($v <=30)):
+					$this->D = 2;
+					break;
+				case ((31 <= $v)&&($v <=40)):
+					$this->D = 3;
+					break;
+				case ((41 <= $v)&&($v <=50)):
+					$this->D = 4;
+					break;
+				case ((51 <= $v)&&($v <=60)):
+					$this->D = 5;
+					break;
+			}
+		}
+	}
