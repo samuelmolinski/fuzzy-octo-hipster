07334d3999ee159e2d1fd9021f0a980507c39865
samuelmolinski <sjmolinski@gmail.com>
2012-10-31 02:54:12 -0200

    -added unit testing for Number class

---
+++ combination/_unitTesting/AllTests.php
@@ -0,0 +1,17 @@
+<?php
+
+
+require_once(ABSPATH.'simpletest/autorun.php');
+/**
+ * Description of all_test
+ *
+ * @author Samuel Molinski <sjmolinski@gmail.com>
+ */
+class AllTests extends TestSuite {
+    //put your code here
+    public function AllTests() {
+        $this->TestSuite('All tests');
+    }
+}
+
+?>
---
+++ combination/test.php
@@ -0,0 +1,26 @@
+<?php
+	
+	set_time_limit(0);
+
+	if (!defined('ABSPATH')) {
+		define('ABSPATH', dirname(__FILE__) . '/');
+	}
+	
+	if (!defined('TESTPATH')) {
+		define('TESTPATH', dirname(__FILE__) . '/_unitTesting/');
+	}
+
+	//required files
+	require_once ('../m_toolbox/m_toolbox.php');
+	require_once (TESTPATH .'AllTests.php');
+
+	start();
+
+	function start() {
+		$test = new AllTests();	
+		
+        $test->addFile(TESTPATH .'CombinationGeneratorTest.php');
+        $test->addFile(TESTPATH .'NumberTest.php');
+	}
+
+?>
