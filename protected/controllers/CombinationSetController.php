c036b20816adefdacb505df154f550ac367cd1e3
samuelmolinski <sjmolinski@gmail.com>
2012-11-27 14:36:48 -0200

    -fixed roles on comb-set controller

--- protected/controllers/CombinationSetController.php
+++ protected/controllers/CombinationSetController.php
@@ -29,15 +29,18 @@ class CombinationSetController extends Controller
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
 				'actions'=>array('index','view', 'check'),
-				'users'=>array('*'),
+				//'users'=>array('*'),
+				'roles'=>array('admin','authenticated'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				//'users'=>array('@'),
+				'roles'=>array('admin'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				//'users'=>array('admin'),
+				'roles'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
